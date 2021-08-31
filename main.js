4a035cc8b3b7e9e5e577dfa4716e6cd941c304cc
ALISSA GROTHE <77845965+grothea@users.noreply.github.com>
2021-08-31 11:47:39 -0600

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-
- //hamburger menu toggle
-function classToggle() {
-  const navs = document.querySelectorAll('.navbar-items');
-  const dropdownDiv = document.getElementById('dropdown');
-  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-  }
-document.querySelector('.navbar-toggle').addEventListener('click', classToggle);
